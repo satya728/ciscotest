e0bed8b20b19ab12e14bf0e0bd3d76ea3e0e66df
satya728 <satya728@gmail.com>
2022-02-20 15:26:37 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,40 @@
-import { Component, Inject } from '@angular/core';
-import { NotesService } from './notes.service';
-import { NewNote, Note } from './note';
+import { Component, Inject } from '@angular/core'
+import { NotesService } from './notes.service'
+import { NewNote, Note } from './note'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  selected?: Note|NewNote;
+    selected: any = {}
+    note: Note
+    list: any = []
 
-  constructor(private readonly notesService: NotesService) {}
+    constructor(private readonly notesService: NotesService) {}
+    ngOninit() {
+        this.newNote()
+    }
 
-  selectNote(note: Note) {
-    // TODO: prevent changing original object
-    this.selected = note;
-  }
+    selectNote(note: Note) {
+        // TODO: prevent changing original object
+        this.selected = note
+    }
 
-  newNote() {
-    this.selected = createNewNote();
-  }
+    newNote() {
+        this.selected = createNewNote()
+    }
 
-  saveNote(note: Note|NewNote) {
-    // TODO: save note
-  }
+    saveNote(note: Note | NewNote) {
+        // TODO: save note
+        this.list.push(note)
+    }
+    getListNotes() {
+        return this.list
+    }
 }
 
 function createNewNote(): NewNote {
-  return {title: '', body: '', color: '', favorite: false};
+    return { title: '', body: '', color: '', favorite: false }
 }
--- src/app/note.ts
+++ src/app/note.ts
@@ -1,14 +1,14 @@
 export interface Note {
-  id: number;
-  title: string;
-  body: string;
-  color: string;
-  favorite: boolean;
+    id: number
+    title: string
+    body: string
+    color: string
+    favorite: boolean
 }
 
 // New note does not have ID yet
-export type NewNote = Omit<Note, 'id'>;
+export type NewNote = Omit<Note, 'id'>
 
-export function isNote(note: Note|NewNote): note is Note {
-  return (note as Note).id != null;
+export function isNote(note: Note | NewNote): note is Note {
+    return (note as Note).id != null
 }
